631b69ed4856f903225a01b4e1896865460f3074
Artem <1ttem12@gmail.com>
2017-02-28 22:17:03 +0100

    Added singleton class DataBase extends Data

---
+++ src/main/java/com/ttem/data/DataBase.java
@@ -0,0 +1,27 @@
+package com.ttem.data;
+
+import com.ttem.dao.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBase extends Data{
+
+    private final static DataBase instance = new DataBase();
+
+    private DataBase() {
+    }
+
+    public static DataBase getInstance() {
+        return DataBase.instance;
+    }
+
+    @Override
+    public User selectUserById(final int id) {
+        return new User(id);
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return new ArrayList<>();
+    }
+}
