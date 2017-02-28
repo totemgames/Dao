b268f3a6a91041c66792ab5f12fc6df35186569a
Artem <1ttem12@gmail.com>
2017-02-28 22:18:53 +0100

    Added singleton class Xml extends Data

---
+++ src/main/java/com/ttem/data/Xml.java
@@ -0,0 +1,27 @@
+package com.ttem.data;
+
+import com.ttem.dao.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Xml extends Data{
+
+    private final static Xml instance = new Xml();
+
+    private Xml() {
+    }
+
+    public static Xml getInstance() {
+        return Xml.instance;
+    }
+
+    @Override
+    public User selectUserById(final int id) {
+        return new User(id);
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return new ArrayList<>();
+    }
+}
