f04870789e732715685235783e037b7446839f43
Artem <1ttem12@gmail.com>
2017-02-28 22:18:24 +0100

    Added singleton class WebService extends Data

---
+++ src/main/java/com/ttem/data/WebService.java
@@ -0,0 +1,25 @@
+package com.ttem.data;
+
+import com.ttem.dao.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebService extends Data{
+
+    private final static WebService instance = new WebService();
+
+    private WebService() {
+    }
+
+    public static WebService getInstance() {
+        return WebService.instance;
+    }
+
+    public User selectUserById(final int id) {
+        return new User(id);
+    }
+
+    public List<User> selectAllUsers() {
+        return new ArrayList<>();
+    }
+}
