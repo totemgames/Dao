bfb7159749af57b9f040cc6e0303c81b4d2b45df
Artem <1ttem12@gmail.com>
2017-02-28 22:12:47 +0100

    Added class User

---
+++ src/main/java/com/ttem/dao/User.java
@@ -0,0 +1,14 @@
+package com.ttem.dao;
+
+public class User {
+
+    private final int id;
+
+    public User(final int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+}
